8fbab16716df88fa8048d8a110b4c237588eca82
Thomas Silva de Farias <thomasfarias19@gmail.com>
2025-05-12 15:57:12 -0300

    feat: Restaurant Listing
    - Add images
    - Install shadcn-ui textarea component
    - Mock restaurants.json
    - Dynamic restaurant listing

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,33 +1,58 @@
+import Image from 'next/image'
+
 import Footer from "../../components/footer";
 import Header from "../../components/header";
 import RestaurantItem from "../../components/restaurant-item";
 
-export default function HomePage() {
+import data from "../../mocks/restaurants.json"
+
+export default async function HomePage() {
   return (
-    <div>
+    <div className='min-h-screen flex flex-col'>
       <Header />
-      <img src="/banner.png" alt="Banner" className="w-full" />
 
-      <h1 className="text-xl font-extrabold text-purple pl-4 pt-6 pb-4">abertos</h1>
-      <RestaurantItem title="Burguer King - Colombo" img="/bk.svg" price="R$6,00" stars={4.7} />
-      <RestaurantItem title="Burguer King - Colombo" img="/bk.svg" price="R$6,00" stars={4.7} />
-      <RestaurantItem title="Burguer King - Colombo" img="/bk.svg" price="R$6,00" stars={4.7} />
-      <RestaurantItem title="Burguer King - Colombo" img="/bk.svg" price="R$6,00" stars={4.7} />
-      <RestaurantItem title="Burguer King - Colombo" img="/bk.svg" price="R$6,00" stars={4.7} />
-      <RestaurantItem title="Burguer King - Colombo" img="/bk.svg" price="R$6,00" stars={4.7} />
-      <RestaurantItem title="Burguer King - Colombo" img="/bk.svg" price="R$6,00" stars={4.7} />
-      <RestaurantItem title="Burguer King - Colombo" img="/bk.svg" price="R$6,00" stars={4.7} />
-      <RestaurantItem title="Burguer King - Colombo" img="/bk.svg" price="R$6,00" stars={4.7} />
-      <RestaurantItem title="Burguer King - Colombo" img="/bk.svg" price="R$6,00" stars={4.7} />
-      <RestaurantItem title="Burguer King - Colombo" img="/bk.svg" price="R$6,00" stars={4.7} />
+      <main className="flex-grow">
+        <Image 
+          src="/banner.png" 
+          alt="Banner promoção de dias das crianças" 
+          width={300}
+          height={200}
+          className="w-full"
+        />
 
-      <h1 className="text-xl font-extrabold text-purple pl-4 pt-8 pb-4">fechados</h1>
+        <h1 className="text-xl font-extrabold text-purple pl-4 pt-6 pb-4">abertos</h1>
 
-      <RestaurantItem title="Burguer King - Colombo" img="/bk.svg" price="R$6,00" stars={4.7} />
-      <RestaurantItem title="Burguer King - Colombo" img="/bk.svg" price="R$6,00" stars={4.7} />
-      <RestaurantItem title="Burguer King - Colombo" img="/bk.svg" price="R$6,00" stars={4.7} />
-      <RestaurantItem title="Burguer King - Colombo" img="/bk.svg" price="R$6,00" stars={4.7} />
+        {data.units.filter(unit => !unit.isClosed).map((unit) => (
+          <RestaurantItem 
+            key={unit.id} 
+            id={unit.id}
+            title={unit.name} 
+            unit={unit.unit}
+            img={unit.logo} 
+            price={unit.shippingPrice} 
+            stars={unit.classification}
+            isClosed={unit.isClosed}
+            isFreeShipping={unit.isFreeShipping}
+          />
+        ))}      
 
+        <h1 className="text-xl font-extrabold text-purple pl-4 pt-8 pb-4">fechados</h1>
+
+        {data.units.filter(unit => unit.isClosed).map((unit) => (
+          <RestaurantItem
+            key={unit.id}
+            id={unit.id}
+            title={unit.name} 
+            unit={unit.unit}
+            img={unit.logo} 
+            price={unit.shippingPrice} 
+            stars={unit.classification}
+            isClosed={unit.isClosed}
+            isFreeShipping={unit.isFreeShipping}
+          />
+        ))}
+      </main>
+      
       <Footer />
     </div>
   )
---
+++ src/components/restaurant-item.tsx
@@ -0,0 +1,88 @@
+'use client'
+
+import { formatPrice } from "@/utils/format-price"
+import Image from "next/image"
+import { useRouter } from "next/navigation"
+
+interface RestaurantItemProps {
+  id: string
+  title: string
+  unit?: string
+  img: string
+  price: number
+  stars: number
+  isClosed?: boolean
+  isFreeShipping?: boolean
+}
+
+export default function RestaurantItem({ 
+  id,
+  title,
+  unit,
+  img,
+  price,
+  stars,
+  isClosed, 
+  isFreeShipping,
+}: RestaurantItemProps) {
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.push(`/${id}`)
+  }
+
+  return (
+    <div       
+      className={`flex gap-3 bg-bg-secondary mx-4 rounded-lg mb-4 cursor-pointer hover:bg-bg-secondary/80 ${
+        isClosed ? 'pointer-events-none' : 'opacity-100'
+      }`}
+      onClick={!isClosed ? handleClick : undefined}
+    >
+      <Image 
+        src={img} 
+        alt={`${title} - ${unit}`}
+        width={72}
+        height={72}
+        className={`${isClosed ? 'opacity-50' : ''}`}
+      />
+
+      <div className="flex flex-col justify-center">
+        <h2 className="text-base font-bold text-text-primary">{title} - {unit}</h2>
+        <div className="flex items-center gap-1">
+          <div className="flex items-center gap-2">
+            {!isFreeShipping ? (
+              <>
+                <Image 
+                  src="/bike.svg" 
+                  alt="Icone preço" 
+                  width={18}
+                  height={13}
+                />
+                <span className="text-water-green-700 text-sm font-bold mr-1">grátis</span>
+              </>
+            ) : (
+              <>
+                <Image 
+                  src="/price-icon.svg" 
+                  alt="Icone preço" 
+                  width={18}
+                  height={13}
+                />
+                <span className="text-purple text-sm font-bold mt-1 mr-1">{formatPrice(price)}</span>
+              </>
+            )}
+          </div>
+          <div className="flex items-center gap-2">
+            <Image 
+              src="/star.svg" 
+              alt="Icone preço" 
+              width={18}
+              height={18}
+            />
+            <span className="text-sm font-bold text-text-secondary mt-1 mr-1">{stars}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
